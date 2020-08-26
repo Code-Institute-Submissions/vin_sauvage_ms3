b75cc890975a1eb1390137031700881cd7726239
yqjapple <[yqjapple@gmail.com](mailto:yqjapple@gmail.com)>
2020-08-27 02:28:45 +0800

    py

--- flask-chat.py
+++ flask-chat.py
@@ -22,6 +22,6 @@ def careers():
     return render_template("careers.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-            port=5000,
+    app.run(host=os.environ.get('IP'),
+            port=os.environ.get('PORT'),
             debug=True)
