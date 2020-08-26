84f12638df38705b5be90254f04348ca7d5b984a
yqjapple <[yqjapple@gmail.com](mailto:yqjapple@gmail.com)>
2020-08-27 02:16:39 +0800

    ip 0000

--- flask-chat.py
+++ flask-chat.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -21,4 +22,6 @@ def careers():
     return render_template("careers.html")
 
 if __name__ == '__main__':
-    app.run();
+    app.run(host='0.0.0.0',
+            port=5000,
+            debug=True)
